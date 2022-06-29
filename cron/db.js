4e3b470ae58f86833bbdd03e250ff7e0c0711b3e
kabilan <kabilan@ncompass.in>
2022-06-29 12:28:12 +0530

    modified database connection

--- cron/db.js
+++ cron/db.js
@@ -2,24 +2,35 @@ const mysql=require('mysql2');
 require('dotenv').config();
 const log=require('simple-node-logger').createSimpleLogger('logs/cron.log');
 
-const connection=()=> {
-        const con=mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        })
-        return con
-}
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  
+  
 
-const queryExecutor = async(sql,params) =>{
-    try {
-        const con = connection();
-        const [result,fields] = await con.promise().query(sql,params)
-        return result
-    } catch (error) {
-        throw error
-    }
+  const queryExecutor =async (query, params) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(query, params, (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                });
+            }
+        });
+    });
 }
 
 module.exports = {
